4846c86c14b6011b58468b677a66126d8a4f22f0
caduunisal <kadu.unisal1@gmail.com>
2025-07-21 09:46:01 -0300

    Add npm packages and dependencies

---
+++ placa-consulta-app/backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 5000;
+app.use(cors());
+
+app.get('/api/consulta', (req, res) => {
+const { placa } = req.query;
+if (placa === 'AAA0000') {
+return res.json({
+placa: 'AAA0000',
+modelo: 'Toyota Corolla',
+ano: '2020',
+cor: 'Prata',
+municipio: 'São Paulo / SP',
+chassi: '9BWZZZ377VT004251'
+});
+
+} else {
+return res.status(404).json({ erro: 'Placa não encontrada' });
+}
+});
+
+app.listen(PORT, () => console.log(`Backend rodando em http://localhost:${PORT}`));
